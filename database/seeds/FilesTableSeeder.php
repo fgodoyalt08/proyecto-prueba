233c577e2d511d98849b640a006b13ab89905bd4
Franco Godoy Alt <Raman@florfresca.com.co>
2019-02-26 20:14:56 -0300

    Modify

--- database/seeds/FilesTableSeeder.php
+++ database/seeds/FilesTableSeeder.php
@@ -13,8 +13,86 @@ class FilesTableSeeder extends Seeder
     public function run()
     {
         DB::table('files')->insert([
-            'title' => "Archivo excel de ejemplo",
-            'description' => "Descripción del archivo",
+            'title' => "Archivo excel de ejemplo 1",
+            'description' => "Descripción del archivo 1",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 2 ",
+            'description' => "Descripción del archivo 2",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 3",
+            'description' => "Descripción del archivo 3",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 4",
+            'description' => "Descripción del archivo 4",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 5",
+            'description' => "Descripción del archivo 5",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 6",
+            'description' => "Descripción del archivo 6",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 7",
+            'description' => "Descripción del archivo 7",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 8",
+            'description' => "Descripción del archivo 8",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 9",
+            'description' => "Descripción del archivo 9",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 0",
+            'description' => "Descripción del archivo 0",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 11",
+            'description' => "Descripción del archivo 11",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 22",
+            'description' => "Descripción del archivo 22",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 33",
+            'description' => "Descripción del archivo 33",
+            'path' => "/example/file.xls",
+        ]);
+
+        DB::table('files')->insert([
+            'title' => "Archivo excel de ejemplo 44 ",
+            'description' => "Descripción del archivo 44",
             'path' => "/example/file.xls",
         ]);
     }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -28,5 +28,61 @@ class UserTableSeeder extends Seeder
 	    $manager->password = bcrypt('admin');
 	    $manager->save();
 	    $manager->roles()->attach($role_admin);
+
+	    $employee = new User();
+	    $employee->name = 'Florencia';
+	    $employee->email = 'Florencia@project.com';
+	    $employee->password = bcrypt('Florencia');
+	    $employee->save();
+	    $employee->roles()->attach($role_user);
+
+	    $employee = new User();
+	    $employee->name = 'Esteban';
+	    $employee->email = 'Esteban@project.com';
+	    $employee->password = bcrypt('Esteban');
+	    $employee->save();
+	    $employee->roles()->attach($role_user);
+
+	    $employee = new User();
+	    $employee->name = 'Franco';
+	    $employee->email = 'Franco@project.com';
+	    $employee->password = bcrypt('Franco');
+	    $employee->save();
+	    $employee->roles()->attach($role_user);
+
+	    $employee = new User();
+	    $employee->name = 'Maria';
+	    $employee->email = 'Maria@project.com';
+	    $employee->password = bcrypt('Maria');
+	    $employee->save();
+	    $employee->roles()->attach($role_user);
+
+	    $employee = new User();
+	    $employee->name = 'Tatiana';
+	    $employee->email = 'Tatiana@project.com';
+	    $employee->password = bcrypt('Tatiana');
+	    $employee->save();
+	    $employee->roles()->attach($role_user);
+
+	    $employee = new User();
+	    $employee->name = 'Tadeo';
+	    $employee->email = 'Tadeo@project.com';
+	    $employee->password = bcrypt('Tadeo');
+	    $employee->save();
+	    $employee->roles()->attach($role_user);
+
+	    $employee = new User();
+	    $employee->name = 'Tomas';
+	    $employee->email = 'Tomas@project.com';
+	    $employee->password = bcrypt('Tomas');
+	    $employee->save();
+	    $employee->roles()->attach($role_user);
+
+	    $employee = new User();
+	    $employee->name = 'Sofia';
+	    $employee->email = 'Sofia@project.com';
+	    $employee->password = bcrypt('Sofia');
+	    $employee->save();
+	    $employee->roles()->attach($role_user);
     }
 }
